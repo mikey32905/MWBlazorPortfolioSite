c320c84cce74cbbf1136466f60290476a1db0497
Mike Williams <mikey32905@gmail.com>
2026-01-09 04:54:00 -0500

    made some minor tweaks.

--- Services/ProjectStateService.cs
+++ Services/ProjectStateService.cs
@@ -6,11 +6,16 @@ namespace MWBlazorPortfolioSite.Services
     {
         public ProjectFile? SelectedFile { get; private set; }
         public event Action? OnChange;
+        // The "Messenger": Components subscribe to this to know when to react
+        public event Action? OnSelectedFileChanged;
 
         public void SelectFile(ProjectFile file)
         {
             SelectedFile = file;
             NotifyStateChanged();
+
+            // Notify all listeners (like the MainLayout menu) that a change happened
+            OnSelectedFileChanged?.Invoke();
         }
 
         private void NotifyStateChanged() => OnChange?.Invoke();
